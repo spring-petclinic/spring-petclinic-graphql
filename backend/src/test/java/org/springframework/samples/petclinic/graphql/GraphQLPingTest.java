6547be79618e86b43d69df1c141c02e7ecd8875d
Nils Hartmann <nils@nilshartmann.net>
2021-07-20 20:48:25 +0200

    add first testcase with authentification

--- backend/src/test/java/org/springframework/samples/petclinic/graphql/GraphQLPingTest.java
+++ backend/src/test/java/org/springframework/samples/petclinic/graphql/GraphQLPingTest.java
@@ -2,45 +2,58 @@ package org.springframework.samples.petclinic.graphql;
 
 //import com.graphql.spring.boot.test.GraphQLResponse;
 //import com.graphql.spring.boot.test.GraphQLTestTemplate;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.graphql.boot.test.tester.AutoConfigureGraphQlTester;
+import org.springframework.graphql.execution.ErrorType;
 import org.springframework.graphql.test.tester.GraphQlTester;
+import org.springframework.graphql.test.tester.WebGraphQlTester;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-/** todo: implement tests, they behave differently than described in spring-graphql docs */
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureGraphQlTester
+/**
+ * todo: implement tests, they behave differently than described in spring-graphql docs
+ */
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@AutoConfigureGraphQlTester
+//@AutoConfigureMockMvc
+
+// https://github.com/spring-projects/spring-graphql/blob/main/samples/webmvc-http/src/test/java/io/spring/sample/graphql/project/MockMvcGraphQlTests.java
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@AutoConfigureWebTestClient
+//@AutoConfigureGraphQlTester
+@SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureGraphQlTester
+
 public class GraphQLPingTest {
+    @Autowired
+    private WebGraphQlTester graphQlTester;
 
-//    @Autowired
-//    private org.springframework.graphql.test.tester.GraphQlTester  graphQlTester;
+    @Test
+    public void meReturnsCurrentUser() {
+        String query = "query { me { username } } ";
+        this.graphQlTester.query(query)
+            .headers(headers -> headers.setBearerAuth("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdXNpIiwiaWF0IjoxNjA4ODg5NDQwLCJleHAiOjIzNjYyNzE4NDB9.XG0SEtHiidGuy2A1zy_BfixVMFOv3gGbfwGqEc3F-KU"))
+            .execute()
+            .path("me.username").entity(String.class).isEqualTo("susi");
 
+    }
 //    @Test
-//    public void pingTest() {
+//    public void unauthorized() {
 //        String query = "query { ping } ";
-//    this.graphQlTester.query(query)
-//        .execute()
-//				.errors()
-//				.satisfy(System.out::println);
+//        this.graphQlTester.query(query)
+//            .execute()
+//            .errors()
+//            .satisfy(errors -> {
+//                assertThat(errors).hasSize(1);
+//                assertThat(errors.get(0).getErrorType()).isEqualTo(ErrorType.UNAUTHORIZED);
+//            });
 //
 //    }
 
-
-//    @Autowired
-//    private GraphQLTestTemplate graphQLTestTemplate;
-//
-//    @Test
-//    void testPing() throws Exception {
-//        GraphQLResponse response = graphQLTestTemplate.
-//            withBearerAuth("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzdXNpIiwiaWF0IjoxNjA4ODg5NDQwLCJleHAiOjIzNjYyNzE4NDB9.XG0SEtHiidGuy2A1zy_BfixVMFOv3gGbfwGqEc3F-KU")
-//            .postForResource("graphql/ping-test.graphql");
-//        assertThat(response.isOk()).isTrue();
-//        assertThat(response.get("$.data.ping")).isEqualTo("pong");
-//    }
 }
