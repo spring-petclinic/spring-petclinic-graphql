a3f69eecb9ea09cff57b9da9a412698ee82f1d04
Nils Hartmann <nils@nilshartmann.net>
2023-10-21 09:36:03 +0200

    Tailwind config

--- frontend-vite/tailwind.config.js
+++ frontend-vite/tailwind.config.js
@@ -1,11 +1,38 @@
+import colors from "tailwindcss/colors"
+import form from "@tailwindcss/forms";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      "spr-white": "#fff",
+      "spr-black": "#191e1e",
+      "spr-gray-dark": "#333",
+      "spr-blue": "#086dc3",
+      "spr-green": "#6bb536",
+      "spr-green-dark": "#458b17",
+      "spr-green-light": "#ebf2f2",
+      "spr-red": "#FF3C38",
+    },
+    fontFamily: {
+      "open-sans": ["Open Sans", "sans-serif"],
+      metro: ["Metropolis", "sans-serif"],
+      helvetica: ["Helvetica", "Arial", "sans-serif"],
+    },
     extend: {},
   },
-  plugins: [],
+  variants: {
+    extend: {
+      cursor: ["disabled"],
+    },
+  },
+  plugins: [form],
 }
