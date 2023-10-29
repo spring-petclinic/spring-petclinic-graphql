c026f2fb2f99c65d20368a9a8f597249d929fbaa
Nils Hartmann <nils@nilshartmann.net>
2023-10-29 17:02:44 +0100

    frontend

--- frontend/patch-index-html.js
+++ frontend/patch-index-html.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const data = fs.readFileSync("./dist/index-orig.html", "utf-8");
+const data = fs.readFileSync("./dist/index.html", "utf-8");
 
 const { BACKEND_HOST } = process.env;
 
